123e5c8e4df5ee1a8484052d7d6a09313bc4a4e8
patrick brisbin <pbrisbin@gmail.com>
2012-05-01 18:56:50 -0400

    move nametrans to pythonfile, add exclude func

---
+++ nametrans.py
@@ -0,0 +1,36 @@
+import re
+
+mapping = { 'INBOX':              'INBOX'
+          , '[Gmail]/All Mail':   'all_mail'
+          , '[Gmail]/Drafts':     'drafts'
+          , '[Gmail]/Important':  'important'
+          , '[Gmail]/Sent Mail':  'sent_mail'
+          , '[Gmail]/Spam':       'spam'
+          , '[Gmail]/Starred':    'starred'
+          , '[Gmail]/Trash':      'trash'
+          }
+
+r_mapping = { val: key for key, val in mapping.items() }
+
+def nt_remote(folder):
+    try:
+        return mapping[folder]
+    except:
+        return re.sub(' ', '_', folder).lower()
+
+def nt_local(folder):
+    try:
+        return r_mapping[folder]
+    except:
+        return re.sub('_', ' ', folder).capitalize()
+
+# folderfilter = exclude(['Label', 'Label', ... ])
+def exclude(excludes):
+    def inner(folder):
+        try:
+            excludes.index(folder)
+            return False
+        except:
+            return True
+
+    return inner
